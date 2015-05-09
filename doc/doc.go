1025cb459d246d34d0584fcdefc8d059857b654a
Your name here <git@adsem.ru>
2015-05-09 20:40:19 +0300

    param

--- doc/doc.go
+++ doc/doc.go
@@ -122,7 +122,7 @@ func (s *Doc) View(func(Tr) Qol) {
 
 // View($)
 const (
-	STRING iota
+	STRICT iota
 	LOWER
 	SYNSET
 	EXPAND_YANDEX
@@ -130,10 +130,42 @@ const (
 	HL_YANDEX
 )
 
-func (s *Qol) {
-	// => [passage_id][]int32{<word_ids>}
-	// passages | strict | summary
-	doc.From(PASSAGES).View(STRICT).With(StatSummary)
+func (s *Qol) Task() {
+	// => [passage_id][]int32{<word_ids>} +++ map[token]count?
+
+	// Common
+	doc.View(SomeParametrisedClosure)
+
+	// Or
+	doc.
+		From(PASSAGES).
+		With(SYNSET).
+		Make(TokenCount). // map[string]int
+		Filter(Frequency(3)).
+		Norm(log).
+		Name("passages.synset.count.log") // auto || from predefined views
+
+	// Qolumn.SetNext(doc.
+
+}
+
+// HDF5 data collection - HANDMADE INSANE CRAP?!!
+func (q *Qolumn) SetNext(qol Qolumn) {
+	name := qol.Name()
+	flat := qol.IsFlat()
+	q.Lock()
+	i := len(q.list)
+	q.list = append(q.list, name)
+	q.flat[i] = flat
+	TypedMakeCopy(q.data[i], qol.data)
+	q.Unlock()
+}
+
+func (q *Qolumn) FromToNew(fromqol, toqol string, f func()) {
+	// SetNext + Doc.Chain()
+}
+
+func DocViewClosure() {
 }
 
 type Qol struct {
